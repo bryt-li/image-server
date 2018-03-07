d2df51954642f7d75f6e63ad5f753fd2a777c695
Bryt Li <bryt@oriente.com>
2018-03-07 19:26:57 +0800

    update

--- dist/client.js
+++ dist/client.js
@@ -1,20 +1,9 @@
 function setImageUrl(text,img){
-  if (text.indexOf("@startuml") == 0) {
+  if (text.indexOf("@startuml") == 0 || 
+    text.indexOf('<?xml version="1.0" encoding="UTF-8"?>') == 0) {
     $.ajax({
       type: "POST",
-      url: "https://utils.orienteexpress.com/image_server/text",
-      data: text,
-      crossDomain: true,
-      dataType: "json"
-    }).done(function(data) {
-      if (data.ok) {
-        img.attr("src", data.payload.url);
-      }
-    });
-  }else if(text.indexOf('<?xml version="1.0" encoding="UTF-8"?>') == 0){
-    $.ajax({
-      type: "POST",
-      url: "https://utils.orienteexpress.com/image_server/text",
+      url: "https://cashaflow.com/image_server/text",
       data: text,
       crossDomain: true,
       dataType: "json"
